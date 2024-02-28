3dc5fc70106f6ed986f52a905fa77f86e56d4bf1
MUNYYBY <hm.muneeb.u.r@gmail.com>
2024-02-29 02:15:30 +0500

    chore: added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { type Metadata } from 'next'
 import { RootLayout } from '@/components/RootLayout'
 
 import '@/styles/tailwind.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: {
@@ -17,6 +18,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-DVX6GV9QGK"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-DVX6GV9QGK')`}
+      </Script>
     </html>
   )
 }
